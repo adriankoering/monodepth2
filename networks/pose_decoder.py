cc24d268c8daf71098d84525a8e5820742e29e3c
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2020-09-18 14:13:52 +0200

    Sequentialize pose decoder

--- networks/pose_decoder.py
+++ networks/pose_decoder.py
@@ -28,30 +28,28 @@ class PoseDecoder(nn.Module):
     self.num_frames_to_predict_for = num_frames_to_predict_for
 
     self.convs = OrderedDict()
-    self.convs[("squeeze")] = nn.Conv2d(self.num_ch_enc[-1], 256, 1)
-    self.convs[("pose", 0)] = nn.Conv2d(num_input_features * 256, 256, 3,
-                                        stride, 1)
-    self.convs[("pose", 1)] = nn.Conv2d(256, 256, 3, stride, 1)
-    self.convs[("pose", 2)] = nn.Conv2d(256, 6 * num_frames_to_predict_for, 1)
-
-    self.relu = nn.ReLU()
-
-    self.net = nn.ModuleList(list(self.convs.values()))
+    self.squeeze = nn.Conv2d(self.num_ch_enc[-1], 256, 1)
+    self.convs = nn.Sequential(
+        nn.ReLU(),
+        nn.Conv2d(num_input_features * 256, 256, 3, stride, 1),
+        nn.ReLU(),
+        nn.Conv2d(256, 256, 3, stride, 1),
+        nn.ReLU(),
+        nn.Conv2d(256, 6 * num_frames_to_predict_for, 1),
+    )
 
   def forward(self, input_features):
+    """ input features is a list with one element: this element has 5 tensors """
+    # last features is a list with one element, where this element contains the
+    # most low-res (most embedded) layer
     last_features = [f[-1] for f in input_features]
 
-    cat_features = [self.relu(self.convs["squeeze"](f)) for f in last_features]
+    # squeezes reduces channel dimensions across last_features
+    cat_features = [self.squeeze(f) for f in last_features]
     cat_features = torch.cat(cat_features, 1)
 
-    out = cat_features
-    for i in range(3):
-      out = self.convs[("pose", i)](out)
-      if i != 2:
-        out = self.relu(out)
-
+    out = self.convs(cat_features)
     out = out.mean(3).mean(2)
-
     out = 0.01 * out.view(-1, self.num_frames_to_predict_for, 1, 6)
 
     axisangle = out[..., :3]
