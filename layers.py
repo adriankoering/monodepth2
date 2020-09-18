4be183e7852bb9b4218a60e47c8f3634ff396f21
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2020-09-18 14:13:07 +0200

    Add comments working through implementation

--- layers.py
+++ layers.py
@@ -80,8 +80,8 @@ class Conv3x3(nn.Module):
 
 
 class BackprojectDepth(nn.Module):
-  """Layer to transform a depth image into a point cloud
-    """
+  """ Layer to transform a depth image into a point cloud
+  """
 
   def __init__(self, batch_size, height, width):
     super(BackprojectDepth, self).__init__()
@@ -107,16 +107,19 @@ class BackprojectDepth(nn.Module):
         torch.cat([self.pix_coords, self.ones], 1), requires_grad=False)
 
   def forward(self, depth, inv_K):
+    # project pixels into rays
     cam_points = torch.matmul(inv_K[:, :3, :3], self.pix_coords)
+    # extend rays into scene
     cam_points = depth.view(self.batch_size, 1, -1) * cam_points
+    # make points into homogeneous coordinates
     cam_points = torch.cat([cam_points, self.ones], 1)
 
     return cam_points
 
 
 class Project3D(nn.Module):
-  """Layer which projects 3D points into a camera with intrinsics K and at position T
-    """
+  """ Layer which projects 3D points into a camera with intrinsics K and at position T
+  """
 
   def __init__(self, batch_size, height, width, eps=1e-7):
     super(Project3D, self).__init__()
@@ -129,12 +132,19 @@ class Project3D(nn.Module):
   def forward(self, points, K, T):
     P = torch.matmul(K, T)[:, :3, :]
 
+    # T @ points : transform points to adjacent frame
+    # K : project points onto image plane
     cam_points = torch.matmul(P, points)
 
+    # remove homogeneous coordinates (leaves x,y)
     pix_coords = cam_points[:, :2, :] / (
         cam_points[:, 2, :].unsqueeze(1) + self.eps)
+
+    # reshape into grid
     pix_coords = pix_coords.view(self.batch_size, 2, self.height, self.width)
     pix_coords = pix_coords.permute(0, 2, 3, 1)
+
+    # normalize into [-1, 1]
     pix_coords[..., 0] /= self.width - 1
     pix_coords[..., 1] /= self.height - 1
     pix_coords = (pix_coords - 0.5) * 2
