0225d1f71c660492c55cf9cd904fddfdf6fb0de5
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2020-09-23 00:38:52 +0200

    Add contextmodules
    (starting with monodepth2 - no context aggregation)

--- plmodels/__init__.py
+++ plmodels/__init__.py
@@ -1,1 +1,2 @@
-from .monodepth import MonoDepth2
+from plmodels.monodepth import MonoDepth2
+from plmodels import contextmodules, depthmodules
---
+++ plmodels/contextmodules.py
@@ -0,0 +1,12 @@
+import torch
+from torch import nn
+
+
+class md2(nn.Module):
+
+  def __init__(self, in_channels, out_channels, pyramid, *args, **kwargs):
+    super().__init__()
+
+  def forward(self, x):
+    return x
+
