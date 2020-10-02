5de514499816bfd1f5399d833da43e4f5f7171ce
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2020-10-02 14:05:26 +0200

    Add downsample + upconv for total stride of 16 (vs 32)

--- plmodels/depthmodules.py
+++ plmodels/depthmodules.py
@@ -82,28 +82,28 @@ class Dorn(nn.Module):
   def __init__(self, context, regression, *args, **kwargs):
     super().__init__()
 
-    ds = [True, True, True, False, False]  # downsample layer0 through layer4
+    ds = [True, True, True, True, False]  # dont downsample last layer
     self.encoder = ResNet(pretrained=True, downsampling=ds, *args, **kwargs)
 
-    # skip upconvs without downsampling
-    f0, f1, f2, _, _ = self.encoder.out_channels
-    u0, u1, u2, _, _ = ds
+    f0, f1, f2, f3, f4 = self.encoder.out_channels
+    u0, u1, u2, u3, u4 = ds
 
-    self.context = getattr(contextmodules, context)(f2, f2, *args, **kwargs)
+    self.context = getattr(contextmodules, context)(f4, 128, *args, **kwargs)
 
+    # skip upconv without downsampling
     # self.upconv4 = DecoderLayer(f4, 256, f3, u4, output=False)
-    # self.upconv3 = DecoderLayer(256, 128, f2, u3, regression=regression)
-    self.upconv2 = DecoderLayer(f2, 64, f1, u2, regression=regression)
+    self.upconv3 = DecoderLayer(f4 + 128, 128, f2, u3, regression=regression)
+    self.upconv2 = DecoderLayer(128, 64, f1, u2, regression=regression)
     self.upconv1 = DecoderLayer(64, 32, f0, u1, regression=regression)
     self.upconv0 = DecoderLayer(32, 16, 0, u0, regression=regression)
 
   def forward(self, images):
     f0, f1, f2, f3, f4 = self.encoder(images)
-
     f4 = self.context(f4)
 
-    x, o2 = self.upconv2(f4, f1)
+    x, o3 = self.upconv3(f4, f2)
+    x, o2 = self.upconv2(x, f1)
     x, o1 = self.upconv1(x, f0)
     _, o0 = self.upconv0(x)
 
-    return o2, o1, o0
+    return o3, o2, o1, o0
