20df4e31b8bda01272f01b6e4e5f6e6cd644f4e6
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2020-10-02 14:05:01 +0200

    Shrink context modules to keep computation in check

--- plmodels/contextmodules.py
+++ plmodels/contextmodules.py
@@ -1,81 +1,60 @@
 import torch
 from torch import nn
+from torch.nn import functional as F
 
 
-class md2(nn.Module):
-
-  def __init__(self, in_channels, out_channels, pyramid, *args, **kwargs):
-    super().__init__()
-
-  def forward(self, x):
-    return x
-
-
-class aspp(nn.Module):
+class ASPP(nn.Module):
 
   def __init__(self,
                in_channels,
                out_channels,
-               pyramid=[6, 12, 18],
+               pyramid=[2, 4, 8],
                *args,
                **kwargs):
     super().__init__()
 
-    out_channels = out_channels // 4
+    l0, l1 = pyramid
+    out_channels //= 2
 
-    l0, l1, l2 = pyramid
+    self.cn0 = nn.Conv2d(in_channels, out_channels, 3, padding=l0, dilation=l0)
+    self.bn0 = nn.BatchNorm2d(out_channels)
 
-    self.cn1 = nn.Conv2d(in_channels, out_channels, 1)
+    self.cn1 = nn.Conv2d(in_channels, out_channels, 3, padding=l1, dilation=l1)
     self.bn1 = nn.BatchNorm2d(out_channels)
-
-    self.cn6 = nn.Conv2d(in_channels, out_channels, 3, padding=l0, dilation=l0)
-    self.bn6 = nn.BatchNorm2d(out_channels)
-
-    self.cn12 = nn.Conv2d(in_channels, out_channels, 3, padding=l1, dilation=l1)
-    self.bn12 = nn.BatchNorm2d(out_channels)
-
-    self.cn18 = nn.Conv2d(in_channels, out_channels, 3, padding=l2, dilation=l2)
-    self.bn18 = nn.BatchNorm2d(out_channels)
 
   def forward(self, x):
 
+    o0 = self.bn0(self.cn0(x)).relu()
     o1 = self.bn1(self.cn1(x)).relu()
-    o6 = self.bn6(self.cn6(x)).relu()
-    o12 = self.bn12(self.cn12(x)).relu()
-    o18 = self.bn18(self.cn18(x)).relu()
 
-    return torch.cat([o1, o6, o12, o18], 1)
+    return torch.cat([x, o0, o1], 1)
 
 
-class psp(nn.Module):
+class PSP(nn.Module):
 
   def __init__(self, in_channels, out_channels, pyramid, *args, **kwargs):
-
-    out_channels = out_channels // 3
+    super().__init__()
 
     p2, p1 = pyramid
-
-    self.conv = nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, 1),
-        nn.BatchNorm2d(out_channels),
-    )
+    out_channels //= 2
 
     self.pool2 = nn.Sequential(
         nn.AdaptiveAvgPool2d((p2, p2)),
         nn.Conv2d(in_channels, out_channels, 1),
         nn.BatchNorm2d(out_channels),
+        nn.ReLU(),
     )
 
     self.pool1 = nn.Sequential(
         nn.AdaptiveAvgPool2d((p1, p1)),
         nn.Conv2d(in_channels, out_channels, 1),
         nn.BatchNorm2d(out_channels),
+        nn.ReLU(),
     )
 
   def forward(self, x):
     B, C, H, W = x.shape
 
-    oc = self.conv(x)
     o2 = F.interpolate(
         self.pool2(x),
         size=(H, W),
@@ -89,4 +68,20 @@ class psp(nn.Module):
         align_corners=True,
     )
 
-    return torch.cat([oc, o2, o1], dim=1)
+    return torch.cat([x, o2, o1], dim=1)
+
+
+class Dorn(nn.Module):
+
+  def __init__(self, in_channels, out_channels, pyramid, *args, **kwargs):
+    super().__init__()
+
+    self.aspp = ASPP(in_channels, out_channels, pyramid)
+    self.psp = PSP(in_channels, out_channels, [1, 4])
+
+  def forward(self, x):
+
+    a = self.aspp(x)
+    p = self.psp(x)
+
+    return torch.cat([a, p], 1)
